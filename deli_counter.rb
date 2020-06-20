15722ca4e7486afbc0aedf15942b6355f9a35481
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-06-20 19:30:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,5 +2,5 @@
 
 katz_deli =[]
 def take_a_number(a,b)
-  array.each_with_index {|element| puts "The line is currently empty."}
+  array.each_with_index {|a,index| puts "The line is currently empty."}
   
