835d2d59479a27c2b9019b47352f69184893c0e2
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-06-20 19:41:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,16 @@
 # Write your code here.
 
 katz_deli =[]
+
+def line(katz_dely)
+end
+
 def take_a_number(katz_dely,name)
   array.each_with_index {|a,index| puts "The line is currently empty."}
 end
+
+def now_serving(katz_dely)
+end
+
+
+  
