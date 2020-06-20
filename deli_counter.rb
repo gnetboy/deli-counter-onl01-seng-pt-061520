6f2a82467b6ca0cda97e2f16952095c78b20367a
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-06-20 19:46:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ katz_deli =[]
 
 def take_a_number(katz_dely,name)
   katz_dely =[]
-  katz_dely.each_with_index {|item,index| puts "The line is currently empty."}
+  katz_dely.each_with_index {|item,index| puts "Welcome, #{item}. You are number #{index+1} in line."}
 end
 
 def line(katz_dely
