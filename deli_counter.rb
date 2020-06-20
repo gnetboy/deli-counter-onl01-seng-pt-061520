57e283c033eeb01480b3d8fbab2f9d22962e9bb1
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-06-20 21:52:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,14 @@
-
-
 def line(array)
-  person_line = []
-  if array.length ==0
-    puts"The line is currently empty."
+   person_line = []
+   if array.length ==0
+      puts"The line is currently empty."
   elsif
       array.each.with.index(1) do |name,index|
       person_line.push("#{index}.#{name}")
   end
     puts "The line is currently : #{line_array.join(" ")}"
+ end
+
 end
 
 
@@ -25,7 +25,6 @@ def now_serving(katz_dely)
     puts "currently serving #{array[0]}."
     array.shift
   end
-end
 
 
   
