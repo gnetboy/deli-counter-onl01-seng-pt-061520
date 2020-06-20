6d0617b670c649b6ee0f015df464ab40b6971caf
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-06-20 21:20:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,17 @@
 
 katz_deli =[]
 
+def line(x)
+  person_line= []
+  if x.length ==0
+    puts"The line is curretly empty."
+  else 
+    x.
+  
+  end
+end
+
+
 
 def take_a_number(array,name)
   katz_dely.each_with_index {|item,index| katz_dely << "Welcome, #{name}. You are number #{index+1} in line."}
